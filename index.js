eb9b0f1a1feda63cfb7faa59785dc1201802e320
Jake Ruth <42501420+jake-ruth@users.noreply.github.com>
2020-11-23 12:15:58 -0500

    Sequelize Boilerplate setup

--- index.js
+++ index.js
@@ -4,21 +4,50 @@ const { Sequelize } = require("sequelize");
 // Option 2: Passing parameters separately (sqlite)
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "paths/to/database.sqlite"
+  storage: "database.sqlite"
 });
+
+//const User = sequelize.import("models/user")(sequelize, Sequelize);
+const models = require("./models"); //path of models folder
+const User = models.User;
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
+    hello2: String
+    readAllTest: [User]
+  }
+  type Mutation {
+    insertTest: User
+  }
+  type User {
+    firstName: String
+    lastName: String
+    email: String
   }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: (root, args, context) => "Hello world!"
-    // hello2: (root, args, context) => "Whatup world!"
+    hello: (root, args, context) => "Hello world!",
+    hello2: (root, args, context) => "Whatup world!",
+    readAllTest: async (root, args, context) => {
+      const allUsers = await User.findAll();
+
+      return allUsers;
+    }
+  },
+  Mutation: {
+    insertTest: async (root, args, context) => {
+      await sequelize.sync();
+      const jane = await User.create({
+        firstName: "Jimmy",
+        lastName: "Doe"
+      });
+      console.log("auto-generated ID:", jane.id);
+    }
   }
 };
 
